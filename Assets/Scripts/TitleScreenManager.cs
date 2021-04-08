15a4c061c7b577345af526f7426786ed76c0e28e
lichengz <whitezhang@live.com>
2021-04-08 11:25:55 +0800

    chair & lamp

--- Assets/Scripts/TitleScreenManager.cs
+++ Assets/Scripts/TitleScreenManager.cs
@@ -9,11 +9,36 @@ public class TitleScreenManager : MonoBehaviour
 {
     public GameObject loadingScreen;
     public Slider slider;
+    // [SerializeField]
+    // [Range(0, 1)]
+    // float progressAnimationMultiplier = 0.25f;
+    // AsyncOperation loadOperation;
+    // float currentValue = 0;
+    // float targetValue = 0;
+    // bool startLoading = false;
+
+    /// <summary>
+    /// Update is called every frame, if the MonoBehaviour is enabled.
+    /// </summary>
+    void Update()
+    {
+        // if (!startLoading) return;
+        // targetValue = loadOperation.progress / 0.9f;
+        // currentValue = Mathf.MoveTowards(currentValue, targetValue, progressAnimationMultiplier * Time.deltaTime);
+        // slider.value = currentValue;
+        // if (Mathf.Approximately(currentValue, 1))
+        // {
+        //     loadOperation.allowSceneActivation = true;
+        // }
+    }
 
     public void LoadGameScene()
     {
         loadingScreen.SetActive(true);
         StartCoroutine(GetSceneLoadProgress());
+        // loadOperation = SceneManager.LoadSceneAsync((int)SceneIndexes.GAME);
+        // loadOperation.allowSceneActivation = false;
+        // startLoading = true;
     }
 
     public IEnumerator GetSceneLoadProgress()
@@ -22,7 +47,7 @@ public class TitleScreenManager : MonoBehaviour
         AsyncOperation operation = SceneManager.LoadSceneAsync((int)SceneIndexes.GAME);
         while (!operation.isDone)
         {
-            slider.value = Mathf.Clamp01(operation.progress);
+            slider.value = Mathf.Clamp01(operation.progress / 0.9f);
             yield return null;
         }
 
